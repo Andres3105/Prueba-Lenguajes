687dd8ff14943fffbbe5d0f1742fb2324317077e
Andres-3105 <andresfbohorquezcortes3105@gmail.com>
2024-10-20 20:11:41 -0500

    Update Program.cs

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,47 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-// Pedimos al usuario que ingrese un número
-Console.Write("Ingresa la cantidad de operaciones que desea realizar: ");
-int cantidad = int.Parse(Console.ReadLine());
-for (int i = 0; i < cantidad; i++)
+﻿class Program
 {
-    Console.Write("Ingresa una opcion de la calculadora:\n1.Suma\n2.Resta\n3.Multiplicacion\n4.Division\n");
-    int opcion = int.Parse(Console.ReadLine());
-    Console.Write("Ingresa el primer numero número: ");
-    int numero1 = int.Parse(Console.ReadLine());
-    Console.Write("Ingresa el primer numero número: ");
-    int numero2 = int.Parse(Console.ReadLine());
-    int resultado = 0;
-    if (opcion == 1)
+    static void main(string[] args)
     {
-        resultado = numero1+numero2;
-        Console.WriteLine($"El resultado de la suma de {numero1} + {numero2} es {resultado}");
-    }
-    else if (opcion == 2)
-    {
-        resultado = numero1-numero2;
-        Console.WriteLine($"El resultado de la resta de {numero1} - {numero2} es {resultado}");
-    }
-    else if (opcion == 3)
-    {
-        resultado = numero1*numero2;
-        Console.WriteLine($"El resultado de la multiplicacion de {numero1} * {numero2} es {resultado}");
-    }
-    else if (opcion == 4)
-    {
-        if (numero2 != 0)
+        Console.Write("Ingresa la cantidad de operaciones que desea realizar: ");
+        int cantidad = int.Parse(Console.ReadLine());
+        for (int i = 0; i < cantidad; i++)
         {
-            resultado = numero1/numero2;
-            Console.WriteLine($"El resultado de la division de {numero1} / {numero2} es {resultado}");
+            Console.Write("Ingresa una opcion de la calculadora:\n1.Suma\n2.Resta\n3.Multiplicacion\n4.Division\n");
+            int opcion = int.Parse(Console.ReadLine());
+            Console.Write("Ingresa el primer numero número: ");
+            int numero1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingresa el primer numero número: ");
+            int numero2 = int.Parse(Console.ReadLine());
+            int resultado = 0;
+            if (opcion == 1)
+            {
+                resultado = numero1+numero2;
+                Console.WriteLine($"El resultado de la suma de {numero1} + {numero2} es {resultado}");
+            }
+            else if (opcion == 2)
+            {
+                resultado = numero1-numero2;
+                Console.WriteLine($"El resultado de la resta de {numero1} - {numero2} es {resultado}");
+            }
+            else if (opcion == 3)
+            {
+                resultado = numero1*numero2;
+                Console.WriteLine($"El resultado de la multiplicacion de {numero1} * {numero2} es {resultado}");
+            }
+            else if (opcion == 4)
+            {
+                if (numero2 != 0)
+                {
+                    resultado = numero1/numero2;
+                    Console.WriteLine($"El resultado de la division de {numero1} / {numero2} es {resultado}");
+                }
+                else
+                {
+                    Console.WriteLine("El numero a dividir debe ser diferente de 0");   
+                }
+        
+            }
+            else
+            {
+                Console.WriteLine("Debe seleccionar un valor asociado a las opciones disponibles");
+            }
         }
-        else
-        {
-            Console.WriteLine("El numero a dividir debe ser diferente de 0");   
-        }
-   
-    }
-    else
-    {
-        Console.WriteLine("Debe seleccionar un valor asociado a las opciones disponibles");
     }
 }
